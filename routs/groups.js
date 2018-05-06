1c38f38190c43408be557b0baecf5aeaf5f04988
Shaham Hai <shahmh@gmail.com>
2018-05-06 18:56:58 +0000

    created get group route

--- routs/groups.js
+++ routs/groups.js
@@ -3,13 +3,37 @@ const { pool } = require("../db/db-connection");
 
 // send all groups
 router.get('/groups', (req, res) => {
-    
+    const text = `
+        SELECT _id, name, (SELECT COUNT(group_id) AS animals FROM animal_group WHERE animal_group.group_id=groups._id) FROM groups;`;
+    let groups = [];
+    pool.connect()
+    .then(client => {
+        return client.query(text)
+        .then(dbRes => {
+            client.release();
+            groups = dbRes.rows.map(row => {
+                return {id: row._id, name: row.name, animals: row.animals};
+                
+            });
+            res.status(200).jsonp({groups:groups});
+        })
+        .catch(e => {
+            client.release();
+            console.error('error in query', e.stack);
+            res.status(400).send();
+        });
+    })
+    .catch(e => {
+        console.error('error in connection', e.stack);
+        res.status(400).send();
+    });
 });
 // create new group
-router.put('/groups', async (req, res) => {
+router.put('/groups', (req, res) => {
     const text = 'INSERT INTO groups(name) VALUES($1)';
     const values = [req.body.name];
-    pool.connect().then(
+    pool.connect()
+    .then(
         client => {
             console.log(client);
             return client.query(text, values)
@@ -20,19 +44,59 @@ router.put('/groups', async (req, res) => {
             .catch(e => {
                 client.release();
                 console.error('error at query', e.stack);
+                res.status(400).send();
             });
         })
         .catch(e => {
             console.error('error at connection',e.stack);
+            res.status(400).send();
         });
     res.status(200).send();
 });
 // edit group
 router.post('/groups', (req, res) => {
-    
+    const { name, id } = req.body;
+    const text = 'UPDATE groups SET name=$2 WHERE _id=$1';
+    const values = [id, name];
+    pool.connect().then(client => {
+        client.query(text, values)
+        .then(dbRes => {
+            client.release();
+            console.log(dbRes.rows);
+            res.status(200).send();
+        })
+        .catch(e => {
+            client.release();
+            console.error('error at query', e.stack);
+        })
+    })
+    .catch(e => {
+        console.error('error at connection', e.stack);
+        res.status(400).send();
+    });
 });
 // delete group
-router.delete('/groups', (req, res) => {
+router.delete('/groups/:id', (req, res) => {
+    const { id } = req.params;
+    const text = 'DELETE FROM groups WHERE _id=$1';
+    const values = [id];
+    pool.connect()
+    .then(client => {
+        client.query(text, values)
+        .then(dbRes => {
+            client.release();
+            res.status(200).send();
+        })
+        .catch(e => {
+            client.release();
+            res.status(400).send();
+            console.error('error in query', e.stack);
+        });
+    })
+    .catch(e => {
+        console.error('error in connection', e.stack);
+        res.status(400).send();
+    });
     
 });
 
